827039378f580bdbea8d536f44e90ce9350c4eec
krystiansledz <krystiansledz419@gmail.com>
2023-04-22 22:48:17 +0200

    Add CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://tnwwkyvl:n91zbcC-sU4ZERFkd3XOIORxS6RPrx3J@isilo.db.elephantsql.com:5432/tnwwkyvl"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
