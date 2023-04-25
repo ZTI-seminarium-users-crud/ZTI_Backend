c7e6b0e0a3e8c8e9be2238af370ee3d8ef3b613b
krystiansledz <krystiansledz419@gmail.com>
2023-04-25 12:53:47 +0200

    Rename Person to Student, rename listStudents/getStudents to readStudents/readStudent

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from api import db
 
 
-class Person(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
--- api/queries.py
+++ api/queries.py
@@ -1,17 +1,17 @@
 from ariadne import convert_kwargs_to_snake_case
 from sqlalchemy import or_
-from api.models import Person
+from api.models import Student
 
 
-def listStudents_resolver(obj, info, limit=10, offset=0, specialization=None, degree=None, semester=None):
+def read_students_resolver(obj, info, limit=10, offset=0, specialization=None, degree=None, semester=None):
     try:
-        query = Person.query
+        query = Student.query
         if specialization:
-            query = query.filter(or_(Person.specialization.in_(specialization), Person.specialization is None))
+            query = query.filter(or_(Student.specialization.in_(specialization), Student.specialization is None))
         if degree:
-            query = query.filter(or_(Person.degree.in_(degree), Person.degree is None))
+            query = query.filter(or_(Student.degree.in_(degree), Student.degree is None))
         if semester:
-            query = query.filter(or_(Person.semester.in_(semester), Person.semester is None))
+            query = query.filter(or_(Student.semester.in_(semester), Student.semester is None))
         students = [student.to_dict() for student in query.slice(offset, offset + limit).all()]
         total_count = query.count()
         current_page = int(offset / limit) + 1
@@ -29,9 +29,9 @@ def listStudents_resolver(obj, info, limit=10, offset=0, specialization=None, de
     return payload
 
 @convert_kwargs_to_snake_case
-def getStudents_resolver(obj, info, id):
+def read_student_resolver(obj, info, id):
     try:
-        student = Person.query.get(id)
+        student = Student.query.get(id)
         payload = {
             "success": True,
             "students": student.to_dict()
